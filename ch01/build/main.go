cae67cfdcf0499b4d1c69cee3535a3d3206b8bb8
Cleberson <pauluci.cleberson@gmail.com>
2024-10-29 23:54:54 -0300

    Examples of Go tools for formatting the code, compile and run the code

---
+++ ch01/build/main.go
@@ -0,0 +1,15 @@
+package main
+
+// Run 'go fmt main.go' to clean up the code formatting.
+// Run 'go build main.go' to build the binary.
+// Run './main' to execute the program.
+
+// 'go run' Compiles and runs a program, without saving an executable file.
+
+// main.go (source code) -> Compiler -> executable file (binary)
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
