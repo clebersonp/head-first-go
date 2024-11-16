b6e5cdc71d3036c1d7b2502a8e1af757fda723eb
Cleberson <pauluci.cleberson@gmail.com>
2024-11-16 00:06:27 -0300

    Example of making Go documentation with ordinary comments

--- ch04/greeting/greeting.go
+++ ch04/greeting/greeting.go
@@ -1,21 +1,31 @@
 // Package greeting isn't "main", it's "greeting"!
+//
+// It has some greetings functions on it.
 package greeting
 
 import "fmt"
 
 // Functions have the first letter capitalized so that functions are exported!
 
+// Hello prints the string "Hello!"
 func Hello() {
 	fmt.Println("Hello!")
 }
 
+// Hi prints the string "Hi!"
 func Hi() {
 	fmt.Println("Hi!")
 }
 
+// AllGreetings prints the strings "Hello!" and "Hi!"
 func AllGreetings() {
 	// we would just type the name functions (without the package name qualifier) because we'd be calling the
 	// functions from the same package where they're defined.
 	Hello()
 	Hi()
 }
+
+// To see the package documentation from terminal:
+// go doc ./ch04/greeting
+// To see the specific function documentation from terminal:
+// go doc ./ch04/greeting Hello
