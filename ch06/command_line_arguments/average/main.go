86e3de692661ef0fa5b8cbaebdc984dd84dbc671
Cleberson <pauluci.cleberson@gmail.com>
2024-11-19 01:24:28 -0300

    Learned about variadic functions, variadic arguments as a slice and passing to a variadic function a slice using 'spread operator'

--- ch06/command_line_arguments/average/main.go
+++ ch06/command_line_arguments/average/main.go
@@ -7,11 +7,24 @@ import (
 	"strconv"
 )
 
+// Variadic functions are functions that can take any number of arguments
+// Variadic must be the last argument of a function
+// We can pass any number of arguments to a variadic function
+// We can pass a slice to a variadic function using the 'spread operator ...'
+// Variadic arguments are passed to the function as a slice
+func average(numbers ...float64) float64 {
+	// create a variable to hold the sum of the numbers
+	var sum float64
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
+
 func main() {
 	// Read command-line arguments
 	arguments := os.Args[1:]
-	// create a variable to hold the sum of the numbers
-	var sum float64
+	var numbers []float64
 	// iterate over the command-line arguments
 	for _, argument := range arguments {
 		// try to convert each command-line argument to a float
@@ -19,11 +32,10 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		// add the converted number to the sum
-		sum += number
+		numbers = append(numbers, number)
 	}
-	// count the number of arguments
-	sampleCount := float64(len(arguments))
+
 	// print the sum and average of the numbers from command-line interface
-	fmt.Printf("Numbers: %#v. Length: %d. Total: %0.2f. Average: %0.2f\n", arguments, len(arguments), sum, sum/sampleCount)
+	// passing a slice to a variadic function using the 'spread operator ...'
+	fmt.Printf("Numbers: %#v. Length: %d. Average: %0.2f\n", arguments, len(arguments), average(numbers...))
 }
