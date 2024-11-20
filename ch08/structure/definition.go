4e24a2b96bac93b15445747f6bea4d5f8c826990
Cleberson <pauluci.cleberson@gmail.com>
2024-11-20 13:45:04 -0300

    Accessing struct's field by the 'dot operator' for both assigning and retrieving them

--- ch08/structure/definition.go
+++ ch08/structure/definition.go
@@ -18,4 +18,12 @@ func main() {
 	// the struct fields, each set to that type's zero value
 	// p%#v will print the value in myStruct as a Go struct literal.
 	fmt.Printf("%#v\n", myStruct)
+
+	// Using the 'dot operator' for both assigning values and retrieving them from structs.
+	myStruct.number = 3.14
+	myStruct.word = "Hello"
+	myStruct.toggle = true
+	fmt.Println(myStruct.number)
+	fmt.Println(myStruct.word)
+	fmt.Println(myStruct.toggle)
 }
