d89e4b78ed184b52a723f73eed92dc79345245c7
Cleberson <pauluci.cleberson@gmail.com>
2024-11-20 09:24:10 -0300

    Init about 'struct' type definition. Declaration directly to a variable and its fields' zero value

---
+++ ch08/structure/definition.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// Arrays, slices and maps are no help if you need to mix values of different types.
+// The can only be set up to hold values of a single type.
+// For that reason, Go provides the 'struct' type to mix different types all together.
+// We use the 'struct' key word to define a structure of many types.
+// Syntax: struct { field1 string, field2 int, ... }
+
+func main() {
+	// declare a struct called myStruct
+	var myStruct struct {
+		number float64
+		word   string
+		toggle bool
+	}
+	// the struct fields, each set to that type's zero value
+	// p%#v will print the value in myStruct as a Go struct literal.
+	fmt.Printf("%#v\n", myStruct)
+}
