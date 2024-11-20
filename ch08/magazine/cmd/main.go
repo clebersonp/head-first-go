29d0d5f502ef4e01d48579fcfe53a3d2a7dfd0bc
Cleberson <pauluci.cleberson@gmail.com>
2024-11-20 17:31:10 -0300

    Creating and initializing a variable of defined type using 'struct literal'

--- ch08/magazine/cmd/main.go
+++ ch08/magazine/cmd/main.go
@@ -12,4 +12,20 @@ func main() {
 	var s magazine.Subscriber
 	s.Rate = 4.99
 	fmt.Println(s.Rate)
+
+	// We can create a variable using 'struct literal'
+	// This is a literal for a Subscriber struct.
+	subscriber := magazine.Subscriber{
+		Name:   "Aman Singh",
+		Rate:   4.99,
+		Active: true,
+	}
+	fmt.Printf("%#v\n", subscriber)
+
+	// We can omit one or more fields in a struct literal.
+	// In that case, all omitted fields will have their zero value
+	newSubscriber := magazine.Subscriber{
+		Rate: 7.90,
+	}
+	fmt.Printf("%#v\n", newSubscriber)
 }
