ae620f0487dcbaf983f1694f0a053f6b0bc538cd
Cleberson <pauluci.cleberson@gmail.com>
2024-11-22 23:23:31 -0300

    Writed some interface definition and examples

--- ch11/gadget/tape.go
+++ ch11/gadget/tape.go
@@ -3,6 +3,19 @@ package gadget
 import "fmt"
 
 // Interface: Two different types that have the same methods. We can define a contract such as an interface.
+// An interface is a set of methods that certain values are expected to have (implement).
+// Any type that has all the methods listed in an interface definition is said to 'satisfy' that interface.
+// A type that satisfies an interface can be used anywhere that interface is called for.
+// A type can have methods in addition to those listed in the interface, but it mustn't be missing any,
+// or it doesn't satisfy that interface.
+// A type can satisfy multiple interfaces, and an interface can (and usually should) have multiple types that satisfy it.
+// Many other languages would require us to explicitly say that SomeType satisfies SomeInterface.
+// But in Go, this happens 'automatically'.
+
+// A concrete type specify the underlying type that holds the value's data.
+// Interface types don't have underlying types. They only describe what a value can do (what methods it has).
+// Syntax: type InterfaceName interface { list of methods }
+// When you have a value in a variable with an interface type, you can only call methods defined as part of that interface.
 
 type TapePlayer struct {
 	Batteries string
