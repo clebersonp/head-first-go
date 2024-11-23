a7643b8b2e90aeffa05c1279527b58ff2c364d18
Cleberson <pauluci.cleberson@gmail.com>
2024-11-23 18:33:54 -0300

    Using the 'defer' keyword to recover from error and ensure that the open resource will be closed

--- ch12/sum/file/file.go
+++ ch12/sum/file/file.go
@@ -22,19 +22,25 @@ func GetFloats(fileName string) ([]float64, error) {
 	if err != nil {
 		return nil, err
 	}
+	// Recovering from errors using deferred functions calls
+	// defer (delay) will execute the CloseFile function before the GetFloats function returns.
+	// It'll be the last thing to execute.
+	// defer execute even any error occurs.
+	// The 'defer' keyword ensures a function call takes place, even if the calling function exits early.
+	// Important: We need to put the call function with defer immediately after the call to 'OpenFile'.
+	defer CloseFile(fl)
 	var numbers []float64
 	scanner := bufio.NewScanner(fl)
 	for scanner.Scan() {
 		var number float64
 		number, err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return nil, err
+			return nil, err // Now, even if an error is returned here, CloseFile will still be called, because 'defer' keyword.
 		}
 		numbers = append(numbers, number)
 	}
-	CloseFile(fl)
 	if scanner.Err() != nil {
-		return nil, scanner.Err()
+		return nil, scanner.Err() // Now, even if an error is returned here, CloseFile will still be called, because 'defer' keyword.
 	}
-	return numbers, nil
+	return numbers, nil // The 'CloseFile' will still be called, because 'defer' keyword and it'll be the last thing to execute.
 }
