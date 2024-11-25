4e6078654b623bc79fa45c54dc732b5713ddb857
Cleberson <pauluci.cleberson@gmail.com>
2024-11-25 17:28:54 -0300

    Refactored the JoinWithCommas function to make it more readable

--- ch14/prose/join.go
+++ ch14/prose/join.go
@@ -8,11 +8,11 @@ func JoinWithCommas(phrases []string) string {
 	if len(phrases) == 1 {
 		return phrases[0]
 	}
-	result := strings.Join(phrases[:len(phrases)-1], ", ")
-	if len(phrases) > 2 {
-		result += ","
+	if len(phrases) == 2 {
+		return phrases[0] + " and " + phrases[1]
 	}
-	result += " and "
+	result := strings.Join(phrases[:len(phrases)-1], ", ")
+	result += ", and "
 	result += phrases[len(phrases)-1]
 	return result
 }
