0e881b96b7822c4b72b178b81aec14b5f87e4d5c
Cleberson <pauluci.cleberson@gmail.com>
2024-11-25 13:39:03 -0300

    Fixed the function for one and two elements to JoinWithComma

--- ch14/prose/join.go
+++ ch14/prose/join.go
@@ -5,8 +5,14 @@ import "strings"
 // Having automated tests is like having your code inspected for bugs automatically every time you make a change!
 
 func JoinWithCommas(phrases []string) string {
+	if len(phrases) == 1 {
+		return phrases[0]
+	}
 	result := strings.Join(phrases[:len(phrases)-1], ", ")
-	result += ", and "
+	if len(phrases) > 2 {
+		result += ","
+	}
+	result += " and "
 	result += phrases[len(phrases)-1]
 	return result
 }
