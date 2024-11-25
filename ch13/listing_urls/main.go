c025e9592cea5b242ecfb9e10f234b59d0e22915
Cleberson <pauluci.cleberson@gmail.com>
2024-11-24 22:13:30 -0300

    Explained a little more about goroutines and much more about channel. Updated the code, added report of duration of execution at the end

--- ch13/listing_urls/main.go
+++ ch13/listing_urls/main.go
@@ -15,6 +15,22 @@ import (
 // An in some situations they allow parallelism: working on multiple tasks simultaneously!
 // The 'main' function of every Go program is started using a goroutine, so every Go program runs at least one goroutine.
 
+// We can't use function return values in a go statement.
+// Go won't let your use the return value from a function called with a go statement, because there's no guarantee
+// the return value will be ready before we attempt to use it.
+// The way to communicate between goroutines is using channels.
+// To declare a variable that holds a channel, you use the 'chan' keyword, followed by the type of values that
+// channel will cary.
+// Syntax: var myChannel chan type
+// To create a channel we need to call the built-in 'make' function.
+// Syntax: var myChannel chan float64 -> Declare a variable to hold a channel.
+//         myChannel = make(chan float64) -> Actually create the channel.
+// Or: myChannel := make(chan float64) -> Create a channel and declare a variable at once.
+// To send a value to channel:
+// Syntax: myChannel <- value
+// To receive a value from channel:
+// Syntax: value := <- myChannel
+
 // We don't directly control when goroutines run. A goroutine run in a different order each time the program is run.
 // But if the order of goroutines run in is important, we need to synchronize them using channels.
 
@@ -22,6 +38,7 @@ import (
 // 		'go' keyword | Function call
 
 func main() {
+	start := time.Now()
 	urls := []string{
 		"https://example.com",
 		"https://golang.org",
@@ -35,6 +52,8 @@ func main() {
 		go responseSize(url)
 	}
 	time.Sleep(time.Second)
+	end := time.Now().Sub(start)
+	fmt.Println("Duration:", end)
 }
 
 func responseSize(url string) {
