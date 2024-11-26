62e9096f970788defcc254032c48dcefd5580b8e
Cleberson <pauluci.cleberson@gmail.com>
2024-11-26 13:10:09 -0300

    Added examples and explanatons about function types and passed-function as a parameter of other function

--- ch15/first-class-citizens/example.go
+++ ch15/first-class-citizens/example.go
@@ -12,6 +12,15 @@ func twice(theFunction func()) {
 	theFunction()
 	theFunction()
 }
+func divide(a, b int) float64 {
+	return float64(a) / float64(b)
+}
+func multiply(a, b int) float64 {
+	return float64(a * b)
+}
+func doMath(a, b int, passedFunction func(int, int) float64) float64 {
+	return passedFunction(a, b)
+}
 
 func main() {
 	var myFunction func()
@@ -22,6 +31,20 @@ func main() {
 	myFunction()
 
 	// First-class functions also allow you to pass functions as arguments to other functions.
+	// Functions type: A function's parameters and return value are part of its type.
 	twice(sayHi)
 	twice(sayBye)
+
+	// declare a variable of type function that has two parameter of type int and return a float64
+	// When we declare a variable of type function, we don't specify the name of parameters, just their types.
+	var mathFunction func(int, int) float64
+	mathFunction = divide
+	fmt.Println(mathFunction(5, 2))
+
+	// Functions that accept a function as a parameter also need to specify the parameters and return types
+	// the passed-in function should have.
+	// If you try to pass a function with the wrong type, Go will alert you to the problem before your program
+	// even compiles.
+	fmt.Println(doMath(6, 3, divide))
+	fmt.Println(doMath(5, 2, multiply))
 }
